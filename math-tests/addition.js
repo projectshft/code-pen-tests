1e21a1796370ba9edb10e9cebe2dcf67c6de528a
amhayslip <amhayslip@gmail.com>
2020-06-25 15:42:03 -0400

    add tests

--- math-tests/addition.js
+++ math-tests/addition.js
@@ -1,7 +1,31 @@
-describe('Add 5 and 10 to make the variable `sum`, 15', function() {
+describe('Create a new variable, `sum`, which is the sum of 5 and 10', function() {
   it('should be a number', function (){
     expect(sum).not.toBeUndefined();
     expect(sum).toBe(15);
     expect(typeof sum).toBe('number');
+  });
+});
+
+describe('Create a new variable, `difference`, which is the result of removing 30 from 60', function() {
+  it('should be a number', function (){
+    expect(difference).not.toBeUndefined();
+    expect(difference).toBe(30);
+    expect(typeof difference).toBe('number');
+  });
+});
+
+describe('Create a new variable, `product`, which is the result of multiplying 6 and 3', function() {
+  it('should be a number', function (){
+    expect(product).not.toBeUndefined();
+    expect(product).toBe(18);
+    expect(typeof product).toBe('number');
+  });
+});
+
+describe('Create a new variable, `quotient`, which is the result of dividing 100 by 5', function() {
+  it('should be a number', function (){
+    expect(quotient).not.toBeUndefined();
+    expect(quotient).toBe(20);
+    expect(typeof quotient).toBe('number');
   });
 });
