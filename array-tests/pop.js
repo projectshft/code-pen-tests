2dfbb6265fd741f90d04ee1f8a817ff2f97dceac
Aaron Hayslip <amhayslip@gmail.com>
2022-03-02 19:48:07 -0500

    Update pop.js

--- array-tests/pop.js
+++ array-tests/pop.js
@@ -2,9 +2,7 @@ beforeEach(function() {
   var cookieIngredients = ['flour', 'salt', 'butter', 'egg', 'vanilla', 'baking soda', 'sugar', 'chocolate chips'];
 }); 
 
-describe('Use `.pop`, to remove the last item in the array (chocolate chips)', function() {
-  var cookieIngredients = ['flour', 'salt', 'butter', 'egg', 'vanilla', 'baking soda', 'sugar', 'chocolate chips'];
-  
+describe('Use `.pop`, to remove the last item in the array (chocolate chips)', function() {  
   it('should remove "chocolate chips"', function () {
     var arrayLength = cookieIngredients.length;
 
